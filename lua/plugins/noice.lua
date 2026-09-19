0cb7b54a42c766030951d6a9365586940ffc1154
lbzeng <lbzeng96@gmail.com>
2026-09-19 16:59:07 +0800

    feat: Add plugin noice.nvim

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,7 @@
+vim.pack.add({
+  "https://github.com/folke/noice.nvim",
+  "https://github.com/MunifTanjim/nui.nvim",
+  "https://github.com/rcarriga/nvim-notify",
+})
+
+require("noice").setup({})
